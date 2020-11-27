649ebc3dc5b8f73652f8ae5eba8ea2412446f552
Moise Andrei <moiseandrei2007@yahoo.com>
2020-11-27 12:25:20 +0200

    fifth commit

--- app/src/main/java/com/example/astm/MainActivity.java
+++ app/src/main/java/com/example/astm/MainActivity.java
@@ -8,6 +8,7 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
@@ -19,8 +20,14 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.*;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.dnn.Net;
 import org.opencv.imgproc.Imgproc;
 
+import org.opencv.dnn.Dnn;
+
+import java.io.File;
 import java.util.Random;
 
 
@@ -28,12 +35,17 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int MY_STORAGE_REQUEST_CODE = 101;
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
 //    int counter = 0; // variable used for mirroring screen in Grayscale
-    boolean startCanny = false;
+//    boolean startCanny = false; //variable used for Edge Detection View
+    boolean startYolo = false;
+    boolean firstTImeYolo = false;
+    Net tinyYolo;
 
-    public void Canny(View Button){
+    // Edge Detection Function for the View
+/*    public void Canny(View Button){
 
         if (startCanny == false){
 
@@ -46,7 +58,36 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             startCanny = false;
 
         }
+    }*/
+
+    public void Yolo(View Button){
+
+        if(startYolo == false){
+
+            startYolo = true;
+
+            if(firstTImeYolo == false){
+
+            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg";
+            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
+
+            File file = new File(tinyYoloCfg);
+            if(file.exists()){
+                Toast.makeText(this, "file exists", Toast.LENGTH_LONG).show();
+            }
+
+            tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
+
+            }
+        }
+
+        else{
+            startYolo = false;
+        }
+
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +98,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_STORAGE_REQUEST_CODE);
         }
 
 
@@ -104,17 +148,30 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         }
 
         counter = counter + 1;*/
+        // End of mirroring screen in Grayscale
+
 
 
         // Edge Detection View
 
-        if (startCanny == true){
+        /*if (startCanny == true){
 
             Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
             Imgproc.Canny(frame, frame, 100, 80);
 
-        }
+        }*/
+        // End of Edge Detection View
 
+        if (startYolo == true){
+
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2RGB);
+
+            Mat imageBlob = Dnn.blobFromImage(frame, 0.00392, new Size(416,416),new Scalar(0, 0, 0),/*swapRB*/false, /*crop*/false);
+
+            tinyYolo.setInput(imageBlob);
+            tinyYolo.forward();
+
+        }
 
         return frame;
     }
@@ -137,7 +194,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onResume();
 
         if (!OpenCVLoader.initDebug()){
-            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"There's a problem!", Toast.LENGTH_SHORT).show();
         }
 
         else
@@ -175,6 +232,13 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+        if(requestCode == MY_STORAGE_REQUEST_CODE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "storage permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "storage permission denied", Toast.LENGTH_LONG).show();
             }
         }
     }
