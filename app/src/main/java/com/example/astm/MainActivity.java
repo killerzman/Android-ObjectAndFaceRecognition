431810c334167e7e311ed9cb809be6a7a4f1eac4
Trăistaru Vlad-Viorel <vladtherocker@gmail.com>
2020-11-27 13:20:20 +0200

    yolov3 initial support added

--- app/src/main/java/com/example/astm/MainActivity.java
+++ app/src/main/java/com/example/astm/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
@@ -28,6 +31,10 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.dnn.Dnn;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Random;
 
 
@@ -35,7 +42,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final int MY_CAMERA_REQUEST_CODE = 100;
-    private static final int MY_STORAGE_REQUEST_CODE = 101;
+    private static final int MY_READ_EXTERNAL_STORAGE_REQUEST_CODE = 101;
+    private static final int MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 102;
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
 //    int counter = 0; // variable used for mirroring screen in Grayscale
@@ -68,13 +76,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
             if(firstTImeYolo == false){
 
-            String tinyYoloCfg = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.cfg";
-            String tinyYoloWeights = Environment.getExternalStorageDirectory() + "/dnns/yolov3-tiny.weights";
-
-            File file = new File(tinyYoloCfg);
-            if(file.exists()){
-                Toast.makeText(this, "file exists", Toast.LENGTH_LONG).show();
-            }
+            String tinyYoloCfg = getApplicationContext().getFilesDir().getPath() + "/dnns/yolov3-tiny.cfg";
+            String tinyYoloWeights = getApplicationContext().getFilesDir().getPath() + "/dnns/yolov3-tiny.weights";
 
             tinyYolo = Dnn.readNetFromDarknet(tinyYoloCfg, tinyYoloWeights);
 
@@ -87,22 +90,69 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     }
 
+    public void copyFileOrDir(String path) {
+        AssetManager assetManager = this.getAssets();
+        String assets[] = null;
+        try {
+            assets = assetManager.list(path);
+            if (assets.length == 0) {
+                copyFile(path);
+            } else {
+                String fullPath = getApplicationContext().getFilesDir().getPath() + "/" + path;
+                File dir = new File(fullPath);
+                if (!dir.exists())
+                    dir.mkdir();
+                for (int i = 0; i < assets.length; ++i) {
+                    copyFileOrDir(path + "/" + assets[i]);
+                }
+            }
+        } catch (IOException ex) {
+            Log.e("tag", "I/O Exception", ex);
+        }
+    }
 
+    private void copyFile(String filename) {
+        AssetManager assetManager = this.getAssets();
+
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = assetManager.open(filename);
+            String newFileName = getApplicationContext().getFilesDir().getPath() + "/" + filename;
+            out = new FileOutputStream(newFileName);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+            out.flush();
+            out.close();
+            out = null;
+        } catch (Exception e) {
+            Log.e("tag", e.getMessage());
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
         }
         if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_STORAGE_REQUEST_CODE);
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_READ_EXTERNAL_STORAGE_REQUEST_CODE);
+        }
+        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
         }
 
+        copyFileOrDir("dnns");
 
         cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
         cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
@@ -234,11 +284,18 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
             }
         }
-        if(requestCode == MY_STORAGE_REQUEST_CODE){
+        if(requestCode == MY_READ_EXTERNAL_STORAGE_REQUEST_CODE){
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "storage permission granted", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "read storage permission granted", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, "storage permission denied", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "read storage permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+        if(requestCode == MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "write storage permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "write storage permission denied", Toast.LENGTH_LONG).show();
             }
         }
     }
